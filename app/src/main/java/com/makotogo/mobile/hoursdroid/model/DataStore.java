c4a4c474c2e83fe7f05f7eb7603859425d83ec5a
J Steven Perry <jstevenperry@gmail.com>
2016-06-28 09:28:27 -0500

    Build 20160628 - Misc bug fixes. Released code under Apache 2.0 license.

--- app/src/main/java/com/makotogo/mobile/hoursdroid/model/DataStore.java
+++ app/src/main/java/com/makotogo/mobile/hoursdroid/model/DataStore.java
@@ -1,3 +1,20 @@
+/*
+ *     Copyright 2016 Makoto Consulting Group, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
 package com.makotogo.mobile.hoursdroid.model;
 
 import android.content.ContentValues;
@@ -469,7 +486,7 @@ public class DataStore {
         // Do the DELETE
         // TODO: check to see if the Job has TimeRecords associated with it. If so, then
         /// TODO: all we can do is mark it deactivated.
-        getDatabase().delete(HoursDbSchema.JobTable.NAME, whereClause, null);
+        getDatabase().delete(HoursDbSchema.ProjectTable.NAME, whereClause, null);
         Log.d(TAG, "DELETE: row ID = " + project.getId());
     }
 
